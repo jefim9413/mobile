e8fe1eb2df679e9364125167884013594d3c9f9a
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-01-24 00:15:58 -0300

    orkut de pobre com APK

---
+++ app/src/main/java/com/example/msgapp/model/Message.kt
@@ -0,0 +1,14 @@
+package com.example.msgapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "messages")
+data class Message(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val content: String,
+    val timeStamp: Long
+)
